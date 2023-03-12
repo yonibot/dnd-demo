3b0dfabb89c832ad59e94d39203f9ab1695227a1
Yoni Weisbrod <yoniweisbrod@Yonis-MacBook-Pro.local>
2023-03-12 14:32:13 +0200

    end of lesson 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 const BLOCK_COLORS = ["blue", "red", "", ""];
+
+function ConditionalBlock({ color }) {
+  const [inDragOver, setInDragOver] = useState(false);
+
+  if (color) return <div draggable className={`block ${color}`}></div>;
+
+  function handleDragOver(e) {
+    e.preventDefault();
+    setInDragOver(true)
+  }
+
+  return (
+    <div
+      className={`block placeholder ${inDragOver ? 'drag-over' : ''}`}
+      onDragOver={handleDragOver}
+    />
+  )
+}
 
 function App() {
   return (
     <div className="container">
       <div className="drag-list">
         {BLOCK_COLORS.map((color) => (
-          <div draggable className={`block ${color}`}></div>
+          <ConditionalBlock color={color} />
         ))}
       </div>
     </div>
